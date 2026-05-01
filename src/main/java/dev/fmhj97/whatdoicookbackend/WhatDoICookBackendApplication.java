4e986926afe91cc92d134b4baa75c36dda361fb7
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2026-05-01 14:11:17 +0200

    First commit

---
+++ src/main/java/dev/fmhj97/whatdoicookbackend/WhatDoICookBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.fmhj97.whatdoicookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatDoICookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatDoICookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/fmhj97/whatdoicookbackend/WhatDoICookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fmhj97.whatdoicookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatDoICookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
