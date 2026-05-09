dc0dc9f635cda71f2dd1b8459bd66907211cacc2
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2026-05-09 18:00:48 +0200

    feat: add Auth DTOs

---
+++ src/main/java/dev/fmhj97/whatdoicookbackend/dto/auth/AuthResponseDto.java
@@ -0,0 +1,4 @@
+package dev.fmhj97.whatdoicookbackend.dto.auth;
+
+public record AuthResponseDto(String token) {
+}
---
+++ src/main/java/dev/fmhj97/whatdoicookbackend/dto/auth/LoginRequestDto.java
@@ -0,0 +1,16 @@
+package dev.fmhj97.whatdoicookbackend.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequestDto(
+
+        @NotBlank(message = "Username is required")
+        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+        String username,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String password
+) {
+}
